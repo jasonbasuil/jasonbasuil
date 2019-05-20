b3d455b82daaeb5a8c74ab09c79dd0261dc19f0e
jasonbasuil <basuilj@gmail.com>
2019-05-20 11:53:47 -0700

    Grid styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, {Component} from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
+import Sidebar from './containers/Sidebar.js'
 import Home from './containers/Home.js'
+import Introduction from './containers/Introduction.js'
 import About from './containers/About.js'
 import Projects from './containers/Projects.js'
 import Contact from './containers/Contact.js'
@@ -14,26 +16,41 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        Jason Basuil's Portfolio Website
-        <Router>
-          <div class='navbar'>
-             <Link exact to="/home">Home</Link>{' '}
-             <Link exact to="/about">About</Link>{' '}
-             <Link exact to="/projects">Projects</Link>{' '}
-             <Link exact to="/contact">Contact</Link>{' '}
+        <div class='side-bar'>
+          <Sidebar />
+        </div>
+        <div class='page'>
+          <div class='main-container'>
+            <Introduction />
+            <About />
+            <Projects />
+            <Contact />
+            <Contact />
+            <Contact />
+            <Contact />
+            <Contact />
+            <Contact />
+            <Contact />
+
           </div>
-
-          <Route path='/home'component={Home}/>
-          <Route path='/about'component={About}/>
-          <Route path='/projects'component={Projects}/>
-          <Route path='/contact'component={Contact}/>
-        </Router>
-
-
-
+        </div>
       </div>
     );
   }
 }
 
 export default App;
+
+// <Router>
+//   <div class='navbar'>
+//      <Link exact to="/home">Home</Link>{' '}
+//      <Link exact to="/about">About</Link>{' '}
+//      <Link exact to="/projects">Projects</Link>{' '}
+//      <Link exact to="/contact">Contact</Link>{' '}
+//   </div>
+//
+//   <Route path='/home'component={Home}/>
+//   <Route path='/about'component={About}/>
+//   <Route path='/projects'component={Projects}/>
+//   <Route path='/contact'component={Contact}/>
+// </Router>
---
+++ src/containers/Sidebar.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react'
+
+class Sidebar extends Component {
+
+  render() {
+    return(
+      <div>
+        <img src=''/>
+        <div>Jason Basuil</div>
+        <div>
+          basuilj@gmail.com
+        </div>
+        <div>
+          (408)-772-3695
+        </div>
+        <div>
+          <a href='https://github.com/jasonbasuil'>Github</a><br/>
+          <a href='https://www.linkedin.com/in/jasonbasuil/'>LinkedIn</a><br/>
+          <a href='https://dev.to/jasonbasuil'>Blog</a>
+        </div>
+
+      </div>
+    )
+  }
+}
+
+export default Sidebar;
