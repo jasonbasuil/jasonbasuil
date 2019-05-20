04942b2db699531a71c183eeee1e3431d92dc3f6
jasonbasuil <basuilj@gmail.com>
2019-05-20 16:10:49 -0700

    Updates

---
+++ precache-manifest.eb6f3cf1b79ec3f5160900ae079c1bd1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66d8973d054254e406faa84046be2f1a",
+    "url": "/jasonbasuil/index.html"
+  },
+  {
+    "revision": "ae28b2821883feb98967",
+    "url": "/jasonbasuil/static/css/main.265226f6.chunk.css"
+  },
+  {
+    "revision": "ef60339c4d2aed56294d",
+    "url": "/jasonbasuil/static/js/2.52a6492a.chunk.js"
+  },
+  {
+    "revision": "ae28b2821883feb98967",
+    "url": "/jasonbasuil/static/js/main.f83c3b1e.chunk.js"
+  },
+  {
+    "revision": "9801f82cbf12ee2f4c9f",
+    "url": "/jasonbasuil/static/js/runtime~main.88423251.js"
+  },
+  {
+    "revision": "888d61682931ce06889ad424caf9edb1",
+    "url": "/jasonbasuil/static/media/IMG_0957.888d6168.png"
+  },
+  {
+    "revision": "a83021f37cf6d01405c9d98d72e9bcd6",
+    "url": "/jasonbasuil/static/media/headshot.a83021f3.JPG"
+  },
+  {
+    "revision": "0a3a0978834655eadc804efda1d3a443",
+    "url": "/jasonbasuil/static/media/iconfinder_mark-github_298822.0a3a0978.svg"
+  },
+  {
+    "revision": "0106ee410e86b324bfa0a342539e356c",
+    "url": "/jasonbasuil/static/media/iconfinder_twitter_834708.0106ee41.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/jasonbasuil/static/media/logo.5d5d9eef.svg"
+  }
+]);
