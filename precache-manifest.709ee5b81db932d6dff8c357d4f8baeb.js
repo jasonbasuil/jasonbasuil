13c2b298755dd5e12768c1eb35ff7a5c2834b06b
jasonbasuil <basuilj@gmail.com>
2020-04-13 14:20:17 -0700

    Updates

---
+++ precache-manifest.709ee5b81db932d6dff8c357d4f8baeb.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a643c3a8ec938c6eba6d79def7a307aa",
+    "url": "/jasonbasuil/index.html"
+  },
+  {
+    "revision": "6c1c5bd9e817692f43f9",
+    "url": "/jasonbasuil/static/css/main.092a3a87.chunk.css"
+  },
+  {
+    "revision": "33f583bf1d97c480df26",
+    "url": "/jasonbasuil/static/js/2.39820cd3.chunk.js"
+  },
+  {
+    "revision": "6c1c5bd9e817692f43f9",
+    "url": "/jasonbasuil/static/js/main.ade1e37c.chunk.js"
+  },
+  {
+    "revision": "9801f82cbf12ee2f4c9f",
+    "url": "/jasonbasuil/static/js/runtime~main.88423251.js"
+  },
+  {
+    "revision": "7f3becc11061fa2432036a34bdb3078d",
+    "url": "/jasonbasuil/static/media/devto.7f3becc1.png"
+  },
+  {
+    "revision": "9ba874743362229f23d10662d3213bd2",
+    "url": "/jasonbasuil/static/media/earnin_logo.9ba87474.png"
+  },
+  {
+    "revision": "0a3a0978834655eadc804efda1d3a443",
+    "url": "/jasonbasuil/static/media/iconfinder_mark-github_298822.0a3a0978.svg"
+  },
+  {
+    "revision": "0106ee410e86b324bfa0a342539e356c",
+    "url": "/jasonbasuil/static/media/iconfinder_twitter_834708.0106ee41.svg"
+  },
+  {
+    "revision": "ef0a2f2671a4e339ee8ad26b8c4eef1c",
+    "url": "/jasonbasuil/static/media/self_snow.ef0a2f26.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c22ed0e4a8dcd7ddd8ad5a027fa27ef6.js"
+  "/jasonbasuil/precache-manifest.709ee5b81db932d6dff8c357d4f8baeb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/jasonbasuil/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
